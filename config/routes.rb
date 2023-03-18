71d0b381abc54f4c08f6bcd546bfb3a653c39c53
rose534 <wambuirose534@gmail.com>
2023-03-18 23:19:44 +0300

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create]
+
+  get '/restaurants', to: 'restaurants#index', defaults: { format: 'json' }
+  get '/restaurants/:id', to: 'restaurants#show', defaults: { format: 'json' }
+  delete '/restaurants/:id', to: 'restaurants#destroy', defaults: { format: 'json' }
+  get '/pizzas', to: 'pizzas#index', defaults: { format: 'json' }
+  post '/restaurant_pizzas', to: 'restaurant_pizzas#create', defaults: { format: 'json' }
 end
