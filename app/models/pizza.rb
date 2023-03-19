d9d9b96d6d10b29f93e73389382b11c30b8babef
rose534 <wambuirose534@gmail.com>
2023-03-19 03:33:05 +0300

    controllers

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,4 +1,8 @@
 class Pizza < ApplicationRecord
     has_many :restaurant_pizzas
     has_many :restaurants, through: :restaurant_pizzas
+
+    def as_json(options={})
+    super(only: [:id, :name, :ingredients])
+  end
 end
